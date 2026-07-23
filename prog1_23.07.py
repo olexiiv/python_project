12a3b5a19f5052f25763a19ecbb386acc8c9446c
olexiiv <vasilyev.alexeyv@gmail.com>
2026-07-23 12:41:48 +0300

    pr1_23.07

---
+++ prog1_23.07.py
@@ -0,0 +1,24 @@
+# player_name = "Нінзя"
+# print(player_name)
+
+player_name = "Маг"
+# print(player_name)
+
+# age_1 = 25
+# print(age_1)
+
+# Age_1 = 30
+# print(Age_1)
+
+# x = 6
+# y = 3
+# z = x + y
+# print(z)
+
+# elmail = "example@example.com"
+# print(elmail)
+
+name = input("Введіть своє ім'я: ")
+print(f"Вітаю, {name}! Радий бачити тебе в нашій грі.")
+
+print(f"З тобою буде грати {player_name}.")
