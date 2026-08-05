94fdb06383f13a469d717c69aef62cf7f0566e0f
olexiiv <vasilyev.alexeyv@gmail.com>
2026-08-06 00:48:32 +0300

    less_04

---
+++ prog4_30.07.py
@@ -0,0 +1,9 @@
+number = int(input('Введи число: '))
+suma = number + 3
+print(f'Ти ввів число: {number}, а після додавання 3 отримаємо: {suma}')
+dobutok = number * 2
+print(f'Ти ввів число: {number}, а після множення на 2 отримаємо: {dobutok}')
+riznytsya = number - 5
+print(f'Ти ввів число: {number}, а після віднімання 5 отримаємо: {riznytsya}')
+chastka = number / 4
+print(f'Ти ввів число: {number}, а після ділення на 4 отримаємо: {chastka}')
