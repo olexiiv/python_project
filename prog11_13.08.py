1ff34a77da534e20f267ed7bf779800bbfc426be
olexiiv <vasilyev.alexeyv@gmail.com>
2026-08-13 13:00:08 +0300

    elif

---
+++ prog11_13.08.py
@@ -0,0 +1,10 @@
+gamer_type = input("Твій гаджет: ")
+
+if gamer_type == "ПК":
+    print("Мишка та клавіатура для тебе!")
+elif gamer_type == "PS":
+    print("Геймпад для тебе!")
+elif gamer_type == "Mobile":
+    print("Граєш будь де!")
+else:
+    print("Ти граєш у настільні ігри! Теж круто!")
